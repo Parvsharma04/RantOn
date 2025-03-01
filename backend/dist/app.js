f959837eda3fc0796f5c52a7db0dbe0fc31c82c7
Parv Sharma <sharmaparv.2004@gmail.com>
2025-03-02 00:55:02 +0530

    Adds Backend structure

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -3,8 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const body_parser_1 = require("body-parser");
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const comments_1 = __importDefault(require("./controllers/comments"));
+const likes_1 = __importDefault(require("./controllers/likes"));
+const rants_1 = __importDefault(require("./controllers/rants"));
+const users_1 = __importDefault(require("./controllers/users"));
 const app = (0, express_1.default)();
+app.use((0, body_parser_1.urlencoded)({ extended: true }));
+app.use((0, cors_1.default)());
+app.use("/api/users", users_1.default);
+app.use("/api/rants", rants_1.default);
+app.use("/api/likes", likes_1.default);
+app.use("/api/comments", comments_1.default);
 app.get("/", (req, res) => {
     res.send("server is running");
 });
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,20 @@
+import { urlencoded } from "body-parser";
+import cors from "cors";
 import express from "express";
+import comments from "./controllers/comments";
+import likes from "./controllers/likes";
+import rants from "./controllers/rants";
+import users from "./controllers/users";
+
 const app = express();
+
+app.use(urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/api/users", users);
+app.use("/api/rants", rants);
+app.use("/api/likes", likes);
+app.use("/api/comments", comments);
 
 app.get("/", (req, res): void => {
   res.send("server is running");
